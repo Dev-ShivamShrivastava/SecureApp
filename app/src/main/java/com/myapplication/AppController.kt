751c92ef6ab36c2e2022f96a53ed4d08e61fc2a6
Shivam Shrivastava <43011013+SS7186805@users.noreply.github.com>
2024-08-22 23:21:29 +0530

    Secure App

---
+++ app/src/main/java/com/myapplication/AppController.kt
@@ -0,0 +1,15 @@
+package com.myapplication
+
+import android.app.Application
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+
+class AppController:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
